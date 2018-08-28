c47fbb460fccf4cb5f685698eb1322465bbf1490
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2018-08-28 19:59:45 +0200

    Redesign to dark theme

--- views/index.js
+++ views/index.js
@@ -3,41 +3,41 @@ var tiles = require('./tiles')
 
 module.exports.list = function (channels) {
   var main = html`<main class="tiles">
-        ${channels.map(channel => tiles.channel(channel))}
-    </main>`
+    ${channels.map(channel => tiles.channel(channel))}
+  </main>`
 
   return body(main)
 }
 
 module.exports.detail = function (threads, channel) {
   var main = html`<main>
-        <pre hidden>${JSON.stringify(threads, null, 2)}</pre>
-        <section class="tiles">${tiles.channel(channel, 'https://twitter.com/' + channel.uri)}</section>
-        ${threads.map(thread => html`<section class="tiles">
-            ${thread.map(status => tiles.status(status, channel.visited))}
-        </section>`)}
-    </main>`
+    <pre hidden>${JSON.stringify(threads, null, 2)}</pre>
+    <section class="tiles">${tiles.channel(channel, 'https://twitter.com/' + channel.uri)}</section>
+    ${threads.map(thread => html`<section class="tiles">
+      ${thread.map(status => tiles.status(status, channel.visited))}
+    </section>`)}
+  </main>`
 
   return body(main)
 }
 
 module.exports.error = function (msg) {
   var main = html`<main>
-        <pre>${msg}</pre>
-    </main>`
+    <pre>${msg}</pre>
+  </main>`
 
   return body(main)
 }
 
 function body (main) {
   return html`<body>
-        ${header()}
-        ${main}
-    </body>`
+    ${header()}
+    ${main}
+  </body>`
 }
 
 function header () {
   return html`<header>
-        <h1><a href="/">Nonlinear</a></h1>
-    </header>`
+    <h1><a href="/">Nonlinear</a></h1>
+  </header>`
 }
--- views/tiles.js
+++ views/tiles.js
@@ -5,36 +5,36 @@ var raw = require('nanohtml/raw')
 module.exports.channel = function (channel, link) {
   link = link || `/twitter/${channel.uri}`
 
-  var content = html`<div>
-        <h2><a href=${link}>${channel.name}</a></h2>
-        <p>${raw(channel.description)}</p>
-    </div>`
+  var content = html`<div class="tile-content${channel.updated > channel.visited ? '--highlighted' : '--regular'}">
+    <h2><a href=${link}>${channel.name}</a></h2>
+    <p>${raw(channel.description)}</p>
+  </div>`
 
-  return tile(channel, content, channel.updated > channel.visited, channel.top)
+  return tile(channel, content, channel.top)
 }
 
 module.exports.status = function (status, visited) {
   var channel = status.channel
-  var content = html`<div>
-        <h2><a href="/twitter/${channel.uri}">${channel.name}</a></h2>
-        <a href="https://twitter.com/${channel.uri}/status/${status.id}">
-            ${date(status.timestamp)}
-        </a>
-        <p>${raw(status.content)}</p>
-    </div>`
+  var content = html`<div class="tile-content${status.updated > visited ? '--highlighted' : '--regular'}">
+    <h2><a href="/twitter/${channel.uri}">${channel.name}</a></h2>
+    <a href="https://twitter.com/${channel.uri}/status/${status.id}">
+      ${date(status.timestamp)}
+    </a>
+    <p>${raw(status.content)}</p>
+  </div>`
 
-  return tile(channel, content, status.updated > visited, false)
+  return tile(channel, content)
 }
 
-function tile (channel, content, highlight, top) {
-  return html`<div class="tile ${highlight ? 'tile-highlight' : ''} ${top ? 'tile-top' : ''}">
-        <img src=${channel.avatar}>
-        ${content}
-    </div>`
+function tile (channel, content, top) {
+  return html`<div class="tile${top ? ' tile-top' : ''}">
+    <img src=${channel.avatar} class="avatar">
+    ${content}
+  </div>`
 }
 
 function date (timestamp) {
   return html`<time>
-        ${moment(timestamp).format('D MMMM YYYY, H:mm')}
-    </time>`
+    ${moment(timestamp).format('D MMMM YYYY, H:mm')}
+  </time>`
 }
